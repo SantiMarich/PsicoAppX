b609d79ffb09d07f2c75fdb46d70bc0cbf303b37
SantiMarich <santiagomarich@gmail.com>
2024-04-05 20:23:37 -0300

    SetupApp

---
+++ frontend/app/page.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Esta es la App</h2>
+      <Button>Boton</Button>
+    </div>
+  );
+}
