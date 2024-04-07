ac9b477c642db270c6dccd805ec53282b1ded88c
SantiMarich <santiagomarich@gmail.com>
2024-04-06 21:24:39 -0300

    Header

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,10 +1,5 @@
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
-  return (
-    <div>
-      <h2>Esta es la App</h2>
-      <Button>Boton</Button>
-    </div>
-  );
+  return <div></div>;
 }
